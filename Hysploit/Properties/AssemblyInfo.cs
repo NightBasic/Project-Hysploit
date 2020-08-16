6a035093bd49c526876080ad9698c9ddf52db520
Ethan Lambert <34895158+NekoHynix@users.noreply.github.com>
2020-08-16 17:17:03 -0600

    Update AssemblyInfo.cs

--- Hysploit/Properties/AssemblyInfo.cs
+++ Hysploit/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Project Hysploit Open Text Build")]
+[assembly: AssemblyTitle("Project Hysploit Open Test Build")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Project Hysploit Open Text Build")]
+[assembly: AssemblyProduct("Project Hysploit Open Test Build")]
 [assembly: AssemblyCopyright("Copyright ©  2019-2020 Neko Yutsu")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
